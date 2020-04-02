9ff02bfb45d3c3fd361b999db7755f783474a250
Amr moussa <amrmoussa2016@icloud.com>
2020-04-03 01:25:44 +0200

    Activity Monitor

---
+++ README.md
@@ -0,0 +1,16 @@
+ ### Activity Monitor
+
+
+ <a href="https://youtu.be/HcL1pQWYn1Q">Activity Monitor Demo</a>
+
+<ul>
+
+<li> Activity monitor watches your daily activities and counts with timer that fires an alarm when activity time runs out  .</li>
+ </ul>
+
+
+
+
+
+
+ <img src="activityMonitor/shot.png" width="400" height="790"> <img src="activityMonitor/shot2.png" width="400" height="790">
--- activityMonitor/ViewController.swift
+++ activityMonitor/ViewController.swift
@@ -7,14 +7,156 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
-
+     var player: AVAudioPlayer!
+   var currntperiod = 0
+    var timer = Timer()
+    var startbutton :UIButton?
+    var edited = false
+       var secondsPassed = 0
+    var textfeild :UITextField?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func activity(_ sender: UITextField) {
+        if let timeTextfeild = self.view.viewWithTag(sender.tag+1) as? UITextField {
+            if !edited  {
+                
+                animatetimertextfeild(timeTextfeild,sender)
+                timeTextfeild.isHidden = false
+                timeTextfeild.text = "⌛"
+                if sender.text == "" {
+                    sender.placeholder = "EX: goal ,activity"
+                }
+                else{
+                    claculateTimeFromText(sender, timeTextfeild)
+                    edited = true
+                    
+                }
+                edited = true
+            }
+            else {
+                edited = false
+                if sender.text == "" {
+                    sender.placeholder = ""
+                     timeTextfeild.isHidden = true
+                }
+                else{
+                    secondsPassed = 0
+                    timer.invalidate()
+                    textfeild = sender
+                    claculateTimeFromText(sender, timeTextfeild)
+                    
+                        
+                    
+                }
+            }
+            
+        }
+        
+        
+    }
+    
+    func animatetimertextfeild(_ uifeild:UITextField,_ activityfeild:UITextField){
+        
+        UIView.animate(withDuration: 0.0, delay: 0.0, options: .curveEaseInOut, animations: {
+            uifeild.transform = CGAffineTransform(translationX:  uifeild.bounds.origin.x + 100, y: uifeild.bounds.origin.y)
+        }, completion: nil)
+        
+        
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: .curveEaseInOut, animations: {
+            uifeild.transform = CGAffineTransform(translationX:  uifeild.bounds.origin.x , y: uifeild.bounds.origin.y)
+        }, completion: nil)
+        
+ 
+        
+        
+    }
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        startbutton = sender
+        secondsPassed = 0
+        timer.invalidate()
+         starttimer()
+        
+        
+    }
+    
+    func claculateTimeFromText(_ acrivityfeild:UITextField,_ timetextfeild:UITextField){
+        
+        let strArr = acrivityfeild.text?.description.split(separator: " ")
 
+    for item in strArr! {
+        let part = item.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
 
+        if let intVal = Int(part) {
+            timetextfeild.text = String(intVal) + " m"
+            currntperiod = intVal
+        }
+    }
+
+        
+        
+    }
+    func  starttimer()  {
+         timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
+        
+    }
+    @objc func updateTimer() {
+         if secondsPassed < currntperiod * 60  {
+             secondsPassed += 1
+//             progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            var currnttime = (Float(secondsPassed) / Float(currntperiod * 60 ))
+//            print(currnttime)
+//            var fra = String((currnttime - Float(Int(currnttime))))
+//            var subindex = fra.index(fra.startIndex, offsetBy: 2)
+//            var index2 = fra.index(fra.startIndex, offsetBy: 4)//will call succ 2 times
+            textfeild?.setGradientBackground(currnttime)
+            let mints = (Float(secondsPassed) / 60.0 )
+//            print(mints)
+//            print(secondsPassed , (Int(mints) * 60))
+//            var  secondss = fra[subindex..<index2]
+            let secondss = (secondsPassed - (Int(mints) * 60))
+            let st = " \(Int(mints)):\(secondss) "
+            startbutton!.setTitle(st, for: .normal )
+            
+         } else {
+             timer.invalidate()
+            startbutton!.setTitle("START",for: .normal)
+             let url = Bundle.main.url(forResource: "D", withExtension: "wav")
+                        player = try! AVAudioPlayer(contentsOf: url!)
+                        player.play()
+                        player.play()
+                        player.play()
+            startbutton!.setTitle("DONE", for: .normal )
+            
+//             let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+//             player = try! AVAudioPlayer(contentsOf: url!)
+//             player.play()
+         }
+     }
+   
+    
 }
-
+extension UIView {
+    func setGradientBackground(_ ratio :Float) {
+      
+            
+        self.layer.sublayers?.remove(at: 0)
+        var  num :NSNumber = NSNumber(value :ratio)
+        print(num)
+        let colorLeft = UIColor.systemGreen.cgColor
+        let colorRight = UIColor.white.cgColor
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorLeft, colorRight]
+        gradientLayer.locations = [ num , num ]
+        gradientLayer.startPoint = CGPoint(x: 1.0 , y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.5)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        }
+   
+}
